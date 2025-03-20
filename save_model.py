249ec043d98ca1488bfdb6defa343d690ad981cb
DESU-CLUB <warrenlowkh@gmail.com>
2025-03-20 08:24:46 +0800

    fix: Fix save.py

--- save_model.py
+++ save_model.py
@@ -5,10 +5,11 @@ api = HfApi()
 
 # Upload the file using your specific repo name
 api.upload_file(
-    path_or_fileobj="trained_encoder_modelpt",  # your local file path
+    path_or_fileobj="trained_encoder_model.pt",  # your local file path
     path_in_repo="trained_encoder_model.pt",  # name of the file in the repo
     repo_id="DESUCLUB/emoji_encoder_elco",  # your specific repo
-    repo_type="model"
+    repo_type="model",
 )
 
 print("Model successfully uploaded to HuggingFace Hub: DESUCLUB/emoji_encoder_elco")
+
